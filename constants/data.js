e9dc2da11d77e2e5820f0ca6b97523ea18272a3a
Omji Kushwaha <omjikush09@gmail.com>
2022-12-09 22:33:45 +0530

    feat:created portfolio"

---
+++ constants/data.js
@@ -0,0 +1,66 @@
+const userData = {
+  githubUsername: "omjikush09",
+  name: "Omji Kushwaha",
+  designation: "Backend Developer",
+  avatarUrl: "/avatar.jpg",
+  email: "omji@omjikush09.com",
+  phone: "+91 XXXXXXXXXX",
+  address: "Prayagraj, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "Online Code Compiler",
+      link: "https:editor.omjikush09.com",
+      imgUrl: "/editor.png",
+    },
+    {
+      title: "Find Developer",
+      link: "https://github.com/omjikush09/FindDev",
+      imgUrl: "/findDev.png",
+    },
+    {
+      title: "Video Chat",
+      link: "https://github.com/omjikush09/video-chat",
+      imgUrl: "/videoChat.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer who loves building products and imporove products",
+    description: [
+      `I am a backend developer who love to scale the produts.`,
+      `I have started my journey as Full Stack developer, later moved on backend development ,enjoying it and learning DevOps.`,
+      `Now I have good experiance with NodeJs, Express, MongoDB,Postgres, C++, docker ,and learning continues... `,
+    ],
+    currentProject: "Onlne Code Compiler",
+    currentProjectUrl: "https://editor.omjikush09.com",
+  },
+  experience: [
+    
+    {
+      title: "Internship",
+      company: "Just Poll",
+      year: "2025",
+      companyLink: "https://justpoll.app/",
+      desc: "Developed Messaging System and worked on Authentication system",
+    },
+    {
+      title: "Graduation",
+      company: "LDC(AKTU), Prayagraj.",
+      year: "2025",
+      companyLink: "https://aktu.ac.in/",
+      desc: "Major in Computer Science and Engineering ",
+    },
+  
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1MGRuXfbNwUOxXXhc6hwx0v7sxpSJaoym/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/omjikush09/",
+    twitter: "https://twitter.com/omjikush09",
+    linkedin: "https://www.linkedin.com/in/omji-kushwaha/",
+    github: "https://github.com/omjikush09",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
